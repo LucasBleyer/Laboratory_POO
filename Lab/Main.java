0d607a65f2c5a09cada869796db6d9f9969abf05
Lucas Bleyer <91075515+LucasBleyer@users.noreply.github.com>
2021-11-18 10:32:40 -0300

    Update Main.java

--- Lab/Main.java
+++ Lab/Main.java
@@ -1,21 +1,20 @@
 public class Lab {
 
     public static void main(String[] args) {
-        Lab pacient1 = new Lab();
-    //pacient   
-        paciente1.setName_pacient("Chuck Berry");
-        paciente1.setNumber_pacient("123.456.789-10");
-        paciente1.setAge_pacient(51);   
-        paciente1.setYear_born(1970);
-        paciente1.setMonth_born(7);
-        paciente1.setDay_born(4);
-        paciente1.setYear_return(2018);
-        paciente1.setMonth_return(4);
-        paciente1.setDay_return(20);
-    //method of calcule of dates and months
-        pacient1.calcule_age_pacient();
-    //show the result with to String
-        System.out.println(pacient1.toString());
-    }
-    
+        Lab paciente1 = new Lab();
+    //dados paciente    
+        paciente1.setNome_paciente("Lucas Bleyer");
+        paciente1.setCpf_paciente("123.456.789-10");
+        paciente1.setIdade_paciente(21);   
+        paciente1.setAno_nasc(2000);
+        paciente1.setMes_nasc(2);
+        paciente1.setDia_nasc(9);
+        paciente1.setAno_retorno(2018);
+        paciente1.setMes_retorno(4);
+        paciente1.setDia_retorno(20);
+    //metodo de calculo das datas
+        paciente1.calculo_idade_paciente();
+    //mostre
+        System.out.println(paciente1.toString());
+    }  
 }
